24eb96c1e9ef93dc543125b5a40b7746f90c631b
Salma Jiménez <sijimenez@unicesar.edu.co>
2026-09-05 20:44:29 -0500

    feat: setup package structure for 4-layer architecture

--- src/main/java/com/mycompany/gamezone/GameZone.java
+++ src/main/java/com/mycompany/gamezone/GameZone.java
@@ -1,16 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.gamezone;
 
-/**
- *
- * @author PC
- */
 public class GameZone {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
 }
