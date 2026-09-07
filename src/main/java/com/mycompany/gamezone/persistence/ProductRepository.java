648030966cc98e0a2e9eecbfdbbcb9a0ce0862c2
Luis <lalejandroguerrero@unicesar.edu.co>
2026-09-07 11:10:18 -0500

    feat: add ProductRepository class structure

---
+++ src/main/java/com/mycompany/gamezone/persistence/ProductRepository.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.gamezone.persistence;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class ProductRepository {
+    private static final String filePath = "products.txt";
+    
+    public void save(){
+        
+    }
+    
+    public void load(){
+        
+    }
+}
+
