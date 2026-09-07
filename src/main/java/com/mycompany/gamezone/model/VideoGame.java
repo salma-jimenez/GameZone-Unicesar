3a9597b7c53438e07f36771c827e4559fc69206a
Luis <lalejandroguerrero@unicesar.edu.co>
2026-09-06 22:59:07 -0500

    feat: implement VideoGame subclass

---
+++ src/main/java/com/mycompany/gamezone/model/VideoGame.java
@@ -0,0 +1,50 @@
+
+package com.mycompany.gamezone.model;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class VideoGame extends Product{
+    private String platform;
+    private String genre;
+    private String ageRating;
+
+    public VideoGame(String platform, String genre, String ageRating, String id, String title, double price, int quantityAvailable) {
+        super(id, title, price, quantityAvailable);
+        this.platform = platform;
+        this.genre = genre;
+        this.ageRating = ageRating;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getAgeRating() {
+        return ageRating;
+    }
+
+    public void setAgeRating(String ageRating) {
+        this.ageRating = ageRating;
+    }
+
+    @Override
+    public String getDescription() {
+        return ("Videojuego: "+ this.getId()+"\nPlataforma: "+this.platform + 
+                "\nGenero: " + this.genre + "\nClasificación de edad: "+ this.ageRating
+                + "\nPrecio: "+ this.getPrice());
+    }
+}
