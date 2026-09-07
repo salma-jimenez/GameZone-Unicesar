01b6de41edb890ed8e79620fbb708f9d5bca24cf
Luis <lalejandroguerrero@unicesar.edu.co>
2026-09-07 11:53:53 -0500

    feat: implement file writing logic in ProductRepository

--- src/main/java/com/mycompany/gamezone/persistence/ProductRepository.java
+++ src/main/java/com/mycompany/gamezone/persistence/ProductRepository.java
@@ -1,6 +1,13 @@
 
 package com.mycompany.gamezone.persistence;
 
+import com.mycompany.gamezone.model.Console;
+import com.mycompany.gamezone.model.Product;
+import com.mycompany.gamezone.model.VideoGame;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.List;
+
 /**
  *
  * @author Alejandro
@@ -8,8 +15,23 @@ package com.mycompany.gamezone.persistence;
 public class ProductRepository {
     private static final String filePath = "products.txt";
     
-    public void save(){
-        
+    public void save(List<Product> products){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            for (Product p : products) {
+                if(p instanceof VideoGame game){
+                    bw.write("VIDEOGAME;"+p.getId() + ";" + p.getTitle() + ";"
+                    + p.getPrice()+";"+p.getQuantityAvailable()+";"+game.getAgeRating()
+                    +";"+game.getGenre()+";"+game.getPlatform());
+                }else if(p instanceof Console console) {
+                    bw.write("CONSOLE;"+p.getId() + ";" + p.getTitle() + ";"
+                    + p.getPrice()+";"+p.getQuantityAvailable()+";"+console.getBrand()
+                    +";"+ console.getGeneration()+";"+console.getModel());
+                }
+                bw.newLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
     public void load(){
