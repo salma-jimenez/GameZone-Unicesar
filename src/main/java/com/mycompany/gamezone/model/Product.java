50bb9471947c6f4512cc1456050bb9efd7ac36ed
Luis <lalejandroguerrero@unicesar.edu.co>
2026-09-06 22:33:03 -0500

    feat: add abstract getDescription method in Product class

--- src/main/java/com/mycompany/gamezone/model/Product.java
+++ src/main/java/com/mycompany/gamezone/model/Product.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.gamezone.model;
 
-/**
- *
- * @author Alejandro
- */
 public class Product {
+    private String id;
+    private String tittle;
+    private double price;
+    private int quantityAvailable;
+
+    public Product(String id, String tittle, double price, int quantityAvailable) {
+        this.id = id;
+        this.tittle = tittle;
+        this.price = price;
+        this.quantityAvailable = quantityAvailable;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantityAvailable() {
+        return quantityAvailable;
+    }
+
+    public void setQuantityAvailable(int quantityAvailable) {
+        this.quantityAvailable = quantityAvailable;
+    }
+    
+    public abstract String getDescription();
     
 }
