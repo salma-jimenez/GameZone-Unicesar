88cd6cfd2d5b8f6610a46a6e75c422e3c8da3513
Salome Isabella Jimenez Vega <sisabellajimenez@unicesar.edu.co>
2026-09-15 21:37:57 -0500

    docs: improve Javadoc for BasicWarranty

--- src/main/java/com/gamezone/model/BasicWarranty.java
+++ src/main/java/com/gamezone/model/BasicWarranty.java
@@ -12,20 +12,37 @@ import java.time.LocalDate;
  */
 public class BasicWarranty extends Warranty {
     
+    /**
+     * Creates a basic warranty starting on the given date.
+     *
+     * @param idWarranty the warranty's unique identifier
+     * @param product the product under warranty (must be a Console)
+     * @param sale the sale associated with this warranty
+     * @param startDate the date the warranty starts (usually the sale date)
+     */
     public BasicWarranty(String idWarranty, Product product, Sale sale, LocalDate startDate) {
         super(idWarranty, product, sale, startDate);
     }
-
+    
+    /**
+     * @return the fixed duration of a basic warranty: 6 months
+     */
     @Override
     public int getDurationInMonths() {
         return 6;
     }
-
+    
+    /**
+     * @return the display name of this warranty type
+     */
     @Override
     public String getWarrantyType() {
         return "Garantía Básica";
     }
-
+    
+     /**
+     * @return the additional cost of a basic warranty: always free
+     */
     @Override
     public Double getAdditionalCost() {
         return 0.0;
